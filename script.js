31b4ac7c6944e180b5d8996971d4e79c8cb6c6e3
object-ions <moshikoatia@gmail.com>
2023-07-27 16:06:19 -0700

    add styling-DOM manip

---
+++ script.js
@@ -0,0 +1,13 @@
+//UI logic:
+const rules = document.getElementById('rules');
+const showRulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+
+//Event listeners
+showRulesBtn.addEventListener('click', function () {
+  rules.classList.add('show');
+});
+
+closeBtn.addEventListener('click', function () {
+  rules.classList.remove('show');
+});
